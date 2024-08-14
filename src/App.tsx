45b7fb7d1506467512ae275830f8378d71a59dfe
Ashish singh <ashishsingh1331@gmail.com>
2024-08-14 18:32:20 +0530

    Typed useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import Child from "./Components/Child";
 import Button from "./Components/Button";
 
 export default function App() {
+  const [value, setValue] = useState<number[]>();
   return (
     <h1>
       <Button outline={true}>HI i am button</Button>
+      <input type="text" value={value} onChange={(e) => setValue([1, 2, 3])} />
     </h1>
   );
 }
